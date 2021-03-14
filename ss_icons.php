d5a24b1eae7394e1e80f7b08ac9a1ae08916976d
Pozitronik <antikillerxxl@gmail.com>
2021-03-14 15:02:14 +0300

    Events icons constants

---
+++ ss_icons.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types = 1);
+
+namespace ss\api;
+
+/**
+ * Class ss_icons
+ * Steel Series API events icon
+ * @package ss\api
+ */
+class ss_icons {
+	public const SS_ICON_BLANK = 0;
+	public const SS_ICON_HEALTH = 1;
+	public const SS_ICON_ARMOR = 2;
+	public const SS_ICON_AMMO = 3;
+	public const SS_ICON_MONEY = 4;
+	public const SS_ICON_FLASH = 5;
+	public const SS_ICON_FLASHBANG = 5;
+	public const SS_ICON_EXPLOSION = 5;
+	public const SS_ICON_KILLS = 6;
+	public const SS_ICON_HEADSHOT = 7;
+	public const SS_ICON_HELMET = 8;
+
+	public const SS_ICON_UNKNOWN_9 = 9;
+
+	public const SS_ICON_HUNGER = 10;
+	public const SS_ICON_AIR = 11;
+	public const SS_ICON_BREATH = 11;
+	public const SS_ICON_COMPASS = 12;
+	public const SS_ICON_TOOL = 13;
+	public const SS_ICON_PICKAXE = 13;
+	public const SS_ICON_MANA = 14;
+	public const SS_ICON_POTION = 14;
+	public const SS_ICON_CLOCK = 15;
+	public const SS_ICON_LIGHTING = 16;
+	public const SS_ICON_ITEM = 17;
+	public const SS_ICON_BACKPACK = 17;
+	public const SS_ICON_AT = 18;
+	public const SS_ICON_MUTED = 19;
+	public const SS_ICON_TALKING = 20;
+	public const SS_ICON_CONNECT = 21;
+	public const SS_ICON_DISCONNECT = 22;
+	public const SS_ICON_MUSIC = 23;
+	public const SS_ICON_PLAY = 24;
+	public const SS_ICON_PAUSE = 25;
+
+	public const SS_ICON_UNKNOWN_26 = 26;
+
+	public const SS_ICON_CPU = 27;
+	public const SS_ICON_GPU = 28;
+	public const SS_ICON_RAM = 29;
+	public const SS_ICON_ASSIST = 30;
+	public const SS_ICON_CREEP_SCORE = 31;
+	public const SS_ICON_DEAD = 32;
+	public const SS_ICON_DRAGON = 33;
+
+	public const SS_ICON_UNKNOWN_34 = 34;
+
+	public const SS_ICON_ENEMIES = 35;
+	public const SS_ICON_GAME_START = 36;
+	public const SS_ICON_GOLD = 37;
+	public const SS_ICON_HEALTH_2 = 38;
+	public const SS_ICON_KILLS_2 = 39;
+	public const SS_ICON_MANA_2 = 40;
+	public const SS_ICON_TEAMMATES = 41;
+	public const SS_ICON_TIMER = 42;
+	public const SS_ICON_TEMPERATURE = 43;
+}
+
